5d185d9ff5e280677b080aa2fd0225461932547f
Phyo Thinzar Aung <phyothinzar.ag92@gmail.com>
2022-08-16 23:25:34 +0630

    Update: Digit Increasing

---
+++ src/DigitIncreasing.java
@@ -0,0 +1,31 @@
+public class DigitIncreasing {
+
+    static int isDigitIncreasing(int n){
+        if(n<10) return 1;
+
+        for(int i=1; i<9; i++){
+            if(n%i==0){
+                StringBuilder stringBuilder = new StringBuilder();
+                int sum = 0;
+                for(int j=0; j<9; j++){
+                    stringBuilder.append(i);
+                    sum += Integer.parseInt(String.valueOf(stringBuilder));
+
+                    if(sum == n){
+                        return 1;
+                    }else if(sum > n){
+                        sum = 0;
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isDigitIncreasing(24));
+        System.out.println(isDigitIncreasing(36));
+        System.out.println(isDigitIncreasing(984));
+        System.out.println(isDigitIncreasing(7404));
+    }
+}
